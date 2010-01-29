9b3ee4fa5737c6cadc7f384f6bc5a186fdf4874f
Corey M. Hoffstein <corey@hoffstein.com>
2010-01-29 13:34:25 -0500

    Updated fit_sentence.cpp to bind sentence to argv[1]

--- GeneticAlgorithm/Fit Sentence/fit_sentence.cpp
+++ GeneticAlgorithm/Fit Sentence/fit_sentence.cpp
@@ -15,6 +15,8 @@
 #include <cstdlib> 
 #include <ctime>
 
+#include <boost/bind.hpp>
+
 #include "population.h"
 #include "evolvable.h"
 
@@ -31,10 +33,8 @@ private:
 	char _c;
 };
 
-std::string sentence = "The quick brown fox jumped over the lazy dog!";
-
 // some random fitness function
-float fitness_function(const Optimization::GeneticAlgorithm::Chromosome<Char>::dna_vector& input) {
+float fitness_function(const std::string& sentence, const Optimization::GeneticAlgorithm::Chromosome<Char>::dna_vector& input) {
 	int sum = sentence.length();
 	for(int i = 0; i < input.size(); i++) {
 		if(input[i].value() == sentence[i])
@@ -47,8 +47,15 @@ float fitness_function(const Optimization::GeneticAlgorithm::Chromosome<Char>::d
 int main (int argc, char * const argv[]) {
 	srand ( time(NULL) );
 	
+	if(argc != 2) { 
+		std::cout << "Usage: ./Fit Sentence <sentence>" << std::endl;
+		exit(0);
+	}
+	
+	std::string sentence(argv[1]);
+	
 	Optimization::GeneticAlgorithm::Population<Char> population(1000, sentence.length());
-	std::pair<float, Optimization::GeneticAlgorithm::Chromosome<Char>::dna_vector> result = population.evolve(&fitness_function, 0.05);
+	std::pair<float, Optimization::GeneticAlgorithm::Chromosome<Char>::dna_vector> result = population.evolve(boost::bind(&fitness_function, sentence, _1), 0.05);
 	
 	std::cout << result.first << ": ";
 	for(int i = 0; i < result.second.size(); i++) {
